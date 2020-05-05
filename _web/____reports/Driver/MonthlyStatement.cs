24e43f7ac9dc932ec2bf2bf496f7f9eff0b917c7
KTLiang <35859602+jiazhiliang@users.noreply.github.com>
2020-05-05 21:39:32 +0930

    Revert "Report"
    This reverts commit fde95f907654d30450a8c724d463981a89767038.

--- _web/____reports/Driver/MonthlyStatement.cs
+++ _web/____reports/Driver/MonthlyStatement.cs
@@ -19,31 +19,8 @@ namespace eTaxi.Reports.Driver
         [Serializable]
         public class DC1
         {
-            public string Ordinal { get; set; }
-            public string Id { get; set; }
             public string Name { get; set; }
-            public string Manager { get; set; }
-            public string PlateNumber { get; set; }
-            public string OpeningBalance { get; set; }
-            public string ClosingBalance { get; set; }
-            public string AdminFee_Amount { get; set; }
-            public string AdminFee_Paid { get; set; }
-            public string Rental_Amount { get; set; }
-            public string Rental_Paid { get; set; }
-            public string Shift_Amount { get; set; }
-            public string Shift_Paid { get; set; }
-            public string Tyre_Amount { get; set; }
-            public string Tyre_Paid { get; set; }
-            public string Violation_Amount { get; set; }
-            public string Violation_Paid { get; set; }
-            public string Fine_Amount { get; set; }
-            public string Fine_Paid { get; set; }
-            public string Bonus_Amount { get; set; }
-            public string Bonus_Paid { get; set; }
-            public string Card_Amount { get; set; }
-            public string Card_Paid { get; set; }
             public string Amount { get; set; }
-            public string Paid { get; set; }
         }
     }
 }
